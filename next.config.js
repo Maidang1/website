6c43304e65ec6b77eefd4093a3f0c7db86e2bc02
maidang1 <497350746@qq.com>
2022-12-28 21:46:16 +0800

    feat: 添加了 UI 库

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    esmExternals: false //  https://github.com/chakra-ui/chakra-ui/issues/7167
+  }
 }
 
 module.exports = nextConfig
