8370e78d1510b55656119cf5f59e563538fc34ce
maidang1 <497350746@qq.com>
2023-03-19 15:40:24 +0800

    修复 mermaid build 问题

--- mdx-custom-components/code.tsx
+++ mdx-custom-components/code.tsx
@@ -1,10 +1,16 @@
 import SyntaxHighlighter from "react-syntax-highlighter";
 import a11yLight from "react-syntax-highlighter/dist/cjs/styles/hljs/a11y-light";
-import { Mermaid } from "mdx-mermaid/lib/Mermaid";
+import dynamic from "next/dynamic";
+
+const DynamicMermaid = dynamic(
+  () => import("mdx-mermaid/lib/Mermaid").then((module) => module.Mermaid),
+  { ssr: false }
+);
+
 export const Code = (props: any) => {
   const { children, className } = props;
   if (className.includes("mermaid")) {
-    return <Mermaid chart={children} />;
+    return <DynamicMermaid chart={children} />;
   }
   return (
     <SyntaxHighlighter style={a11yLight} language="javascript">
--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-const mdxMermaid = require("mdx-mermaid");
+const { mermaidPlugin } = require("./libs/mdx-mermaid.js");
 const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
   options: {
     // If you use remark-gfm, you'll need to use next.config.mjs
     // as the package is ESM only
     // https://github.com/remarkjs/remark-gfm#install
-    remarkPlugins: [[mdxMermaid.default, { output: "svg" }]],
+    remarkPlugins: [[mermaidPlugin, { output: "svg" }]],
     rehypePlugins: [],
     // If you use `MDXProvider`, uncomment the following line.
     providerImportSource: "@mdx-js/react",
