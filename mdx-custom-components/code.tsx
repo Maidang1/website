a982cb93f7fb75d2295b07cf0130d171c43a0962
maidang1 <497350746@qq.com>
2023-03-19 14:48:08 +0800

    集成 mermaid

--- mdx-custom-components/code.tsx
+++ mdx-custom-components/code.tsx
@@ -1,7 +1,11 @@
 import SyntaxHighlighter from "react-syntax-highlighter";
 import a11yLight from "react-syntax-highlighter/dist/cjs/styles/hljs/a11y-light";
+import { Mermaid } from "mdx-mermaid/lib/Mermaid";
 export const Code = (props: any) => {
-  const { children } = props;
+  const { children, className } = props;
+  if (className.includes("mermaid")) {
+    return <Mermaid chart={children} />;
+  }
   return (
     <SyntaxHighlighter style={a11yLight} language="javascript">
       {children}
