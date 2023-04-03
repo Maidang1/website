dfd22bf290773ed9a60d68abf1f048cb979b8d24
maidang1 <497350746@qq.com>
2023-04-03 22:05:35 +0800

    暂时移除 styled-components 修复 SSR 报错

--- mdx-custom-components/header/index.tsx
+++ mdx-custom-components/header/index.tsx
@@ -1,25 +1,9 @@
-import styled from "styled-components";
-
-const StyledH1 = styled.h1`
-  font-weight: 700;
-  margin-top: 24px;
-  margin-bottom: 16px;
-  font-size: 1.5rem /* 24px */;
-  line-height: 2rem /* 32px */;
-`;
-
-const StyledH2 = styled.h2`
-  font-weight: 600;
-  margin-top: 16px;
-  margin-bottom: 12px;
-  font-size: 1.25rem /* 20px */;
-  line-height: 1.75rem /* 28px */;
-`;
+import styles from "./styled.module.css";
 
 export const H1 = ({ children }: any) => {
-  return <StyledH1>{children}</StyledH1>;
+  return <h1 className={styles.h1}>{children}</h1>;
 };
 
 export const H2 = ({ children }: any) => {
-  return <StyledH2>{children}</StyledH2>;
+  return <h2 className={styles.h2}>{children}</h2>;
 };
