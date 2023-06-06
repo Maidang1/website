c820f58a3532e301ef0dc6fb01c817e4ae0faea9
maidang1 <497350746@qq.com>
2023-06-06 22:22:44 +0800

    feat: 添加 rollup 打包

--- data/post.ts
+++ data/post.ts
@@ -1,12 +1,1 @@
-export const postsData = [
-  {
-    title: "博客的来源",
-    summary: "简单介绍一下博客的来源",
-    link: "/jian-dan-jie-shao-yi-xia-bo-ke-de-lai-yuan",
-  },
-  {
-    title: "zustand 原理介绍",
-    link: "/my-mdx-page",
-    summary: "浅浅的阅读了一下 zustand 的原理，简单总结一下吧",
-  },
-];
+export const postsData =[{"title":"博客的来源","summary":"简单介绍一下博客的来源","link":"/jian-dan-jie-shao-yi-xia-bo-ke-de-lai-yuan"},{"title":"zustand 原理介绍","link":"/my-mdx-page","summary":"浅浅的阅读了一下 zustand 的原理，简单总结一下吧"},{"title":"rollup打包记录","summary":"暂无简介","link":"/rollup-da-bao-ji-lu"}]
