5482d3255746be18eb467bf7ec54f88d30a4937b
maidang1 <497350746@qq.com>
2023-06-06 23:08:05 +0800

    feat: 添加视频记录

--- data/post.ts
+++ data/post.ts
@@ -1,1 +1,18 @@
-export const postsData =[{"title":"博客的来源","summary":"简单介绍一下博客的来源","link":"/jian-dan-jie-shao-yi-xia-bo-ke-de-lai-yuan"},{"title":"zustand 原理介绍","link":"/my-mdx-page","summary":"浅浅的阅读了一下 zustand 的原理，简单总结一下吧"},{"title":"rollup打包记录","summary":"暂无简介","link":"/rollup-da-bao-ji-lu"}]
+export const postsData = [
+  {
+    title: "博客的来源",
+    summary: "简单介绍一下博客的来源",
+    link: "/jian-dan-jie-shao-yi-xia-bo-ke-de-lai-yuan",
+  },
+  {
+    title: "zustand 原理介绍",
+    link: "/my-mdx-page",
+    summary: "浅浅的阅读了一下 zustand 的原理，简单总结一下吧",
+  },
+  {
+    title: "rollup打包记录",
+    summary: "记录一下 rollup 打包",
+    link: "/rollup-da-bao-ji-lu",
+  },
+  { title: "直播记录", summary: "记录一下自己的视频", link: "/zhi-bo-ji-lu" },
+];
