623086d2a062baf879235d7052869985e143268b
maidang1 <497350746@qq.com>
2023-06-24 23:58:27 +0800

    remove unused files

--- libs/fm-loader.js
+++
@@ -1,7 +0,0 @@
-const matter = require("gray-matter");
-module.exports = async function (src) {
-  const callback = this.async();
-  const { content, data } = matter(src);
-  console.log({ src, content, data });
-  return callback(null, data);
-};
